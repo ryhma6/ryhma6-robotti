69f9fde13dbc80ef17788299b59b1183af0d4bd7
Tomi Vesanen <vivesanto@student.hamk.fi>
2016-04-19 14:43:07 +0300

    Yhteys luokan toinen implementaatio

--- src/virallinenRobotti6/Yhteys.java
+++ src/virallinenRobotti6/Yhteys.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 import lejos.nxt.LCD;
+import lejos.nxt.Sound;
 import lejos.nxt.comm.NXTConnection;
 import lejos.nxt.comm.USB;
 
@@ -20,12 +21,17 @@ public class Yhteys implements Runnable {
 		isRunning = true;
 	}
 	
-	public void Connect() {
+	public boolean Connect() {
 		while (connection == null)
 		{
-			//connection = Bluetooth.waitForConnection(30, 0);
-			connection = USB.waitForConnection(30, 0);
+			//connection = Bluetooth.waitForConnection(30000, 0);
+			connection = USB.waitForConnection(30000, 0);
 		}
+		
+		if (connection != null)
+			return true;
+		else
+			return false;
 	}
 	
 	public void ReadData() throws IOException {
@@ -45,7 +51,27 @@ public class Yhteys implements Runnable {
 	
 	public void run() {
 		while (isRunning) {
-			
+			if (Connect()) {
+				try {
+					ReadData();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					LCD.clear();
+					LCD.drawString("READ FAIL", 7, 2);
+					Sound.twoBeeps();
+				}
+				
+				try {
+					OutputData();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					LCD.clear();
+					LCD.drawString("OUTPUT FAIL", 7, 2);
+					Sound.twoBeeps();
+				}
+			}
 		}
 	}
 }
